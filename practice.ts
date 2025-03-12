cf38cb403df1c1a75e752f63b06335b9cdb17800
S-MADHAN-KUMAR <madhankumar419@gmail.com>
2025-03-12 18:22:41 +0530

    updating

--- practice.ts
+++ practice.ts
@@ -1,18 +1,22 @@
-// ***Boolean*** //
-// let isActive = true;
-// console.log(isActive) 
-// let isLoggin : boolean;
-// isLoggin = false;
-// console.log(isLoggin);
+//  Boolean
 
-//Number
+let isActive = true;
+console.log(isActive) 
+let isLoggin : boolean;
+isLoggin = false;
+console.log(isLoggin);
+
+//  Number
+
 let value = 10;
 console.log(value);
+
 let val : number;
 val = 25;
 console.log(val);
 
-//String
+//  String
+
 let myName = 'Prabhu'
 console.log(myName);
 
@@ -20,461 +24,489 @@ let person : string;
 person = 'Manikandan'
 console.log(person);
 
-//Void
+//  Void
+
 function test(){
   console.log("Test Mode");
-  
 }
 test()
 
 let check = ()=>{
   console.log("Its Worked");
-  
 }
 check()
 
-//undefined
+//  undefined
+
 let myName : string;
 console.log(myName);
 
-//null
-// let user : string | null = null;
-// console.log(user);
+//  null
 
-// user = 'Prathi'
-// console.log(user);
+let user : string | null = null;
+console.log(user);
 
-//interface
+user = 'Prathi'
+console.log(user);
 
-// interface user{
-//   name : string;
-//   age?:number;
-// }
+//  interface
 
-// interface user{
-//   city: string
-// }
+interface user{
+  name : string;
+  age?:number;
+}
 
-// const myUser : user = {
-//   name : 'Prabhu',
-//   age : 40,
-//   city : 'chennai'
-// }
+interface user{
+  city: string
+}
 
-// console.log(myUser);
-// console.log(myUser.name);
-// myUser.name = 'Mani'
-// console.log(myUser);
+const myUser : user = {
+  name : 'Prabhu',
+  age : 40,
+  city : 'chennai'
+}
 
-// interface person {
-//   name : string
-//   age : number
-//   city : string
-//   lang?: string
-// }
+console.log(myUser);
+console.log(myUser.name);
+myUser.name = 'Mani'
+console.log(myUser);
 
-// const Person : person = {
-//   name : 'Pranav',
-//   age : 7,
-//   city : 'chennai'
-// }
+//===============================
 
-// console.log(Person);
+interface person {
+  name : string
+  age : number
+  city : string
+  lang?: string
+}
 
-//Enum
+const Person : person = {
+  name : 'Pranav',
+  age : 7,
+  city : 'chennai'
+}
 
-// enum orderStatus {
-//   pending = "PENDING",
-//   shipped = "SHIPPED",
-//   delivered = "DELIVERED"
-// }
+console.log(Person);
 
-// function updateOrderStatus(orderID:number, status:orderStatus):string{
-//   return `This Order ${orderID} is now ${status}`
-// }
+//  Enum
 
-// console.log(updateOrderStatus(100,orderStatus.pending));
+enum orderStatus {
+  pending = "PENDING",
+  shipped = "SHIPPED",
+  delivered = "DELIVERED"
+}
 
-//Array
-// let arr : number[] = []
-// arr.push(1)
-// arr.push(2)
-// arr.push(3)
-// arr.push(4)
-// console.log(arr);
+function updateOrderStatus(orderID:number, status:orderStatus):string{
+  return `This Order ${orderID} is now ${status}`
+}
 
-// let array = ['prabhu','mani','shanthi']
-// console.log(array);
-// array[0] = 'Pranav'
-// console.log(array);
-// array.shift()
-// console.log(array);
+console.log(updateOrderStatus(100,orderStatus.pending));
 
-// let person : (string | number | boolean)[];
+//  Array
 
-// person = [10,true,'Prabhu']
-// console.log(person);
+let arr : number[] = []
+arr.push(1)
+arr.push(2)
+arr.push(3)
+arr.push(4)
+console.log(arr);
 
-// const myUser = ['Shanthi',31,'Chennai',true]
+let array = ['prabhu','mani','shanthi']
+console.log(array);
+array[0] = 'Pranav'
+console.log(array);
+array.shift()
+console.log(array);
 
-//Tuple
-// const values : [string,number] = ['Prabhu',10]
-// console.log(values);
-// values[0] = 'Shanthi'
-// values[1] = 30
-// console.log(values);
-// values[0] = 10  // Error
-// values[1] = 'Pranav'  //Error
-// values.push('Mani') // Accepted even tuple
-// values.push(30) // Accepted even tuple
-// console.log(values);
+let person : (string | number | boolean)[];
+
+person = [10,true,'Prabhu']
+console.log(person);
+
+const myUser = ['Shanthi',31,'Chennai',true]
+
+//  Tuple
+
+const values : [string,number] = ['Prabhu',10]
+console.log(values);
+values[0] = 'Shanthi'
+values[1] = 30
+console.log(values);
+values[0] = 10              // Error
+values[1] = 'Pranav'       // Error
+values.push('Mani')        // Accepted even tuple
+values.push(30)            // Accepted even tuple
+console.log(values);
 
 
-//Object
-// const person : {
-//   name : string,
-//   age : number,
-//   city : string
-//   [key:string] : any
-// } = {
-//   name : 'Prabhu',
-//   age : 40,
-//   city:'Chennai'
-// }
+//  Object
 
-// console.log(person);
-// person.lang = 'Tamil'
-// person.pincode = 626142
-// console.log(person);
+const person : {
+  name : string,
+  age : number,
+  city : string
+  [key:string] : any
+} = {
+  name : 'Prabhu',
+  age : 40,
+  city:'Chennai'
+}
 
-// //class
-// class Coder{
-//   public name : string
-//   private age : number
-//   protected language : string
-//   constructor(name:string, age:number, language:string){
-//     this.name = name
-//     this.age = age
-//     this.language = language
-//   }
-//   getAge(){
-//   console.log(`My Age is ${this.age}`);
+console.log(person);
+person.lang = 'Tamil'
+person.pincode = 626142
+console.log(person);
+
+//  class
+
+class Coder{
+  public name : string
+  private age : number
+  protected language : string
+  constructor(name:string, age:number, language:string){
+    this.name = name
+    this.age = age
+    this.language = language
+  }
+  getAge(){
+  console.log(`My Age is ${this.age}`);
   
-//   }
-//   getLang(){
-//     console.log(`I write ${this.language}`);
+  }
+  getLang(){
+    console.log(`I write ${this.language}`);
     
-//   }
-// }
+  }
+}
 
-// const person1 = new Coder('Prabhu',36, 'TypeScript')
-// console.log(person1);
-// person1.name = 'Prathiksha'
-// console.log(person1);
-// //console.log(person1.age)
-// person1.getLang()
+const person1 = new Coder('Prabhu',36, 'TypeScript')
+console.log(person1);
+person1.name = 'Prathiksha'
+console.log(person1);
+console.log(person1.age)
+person1.getLang()
 
-// class WebDev extends Coder{
-//   computer : string
-//   constructor(name:string,age:number,language:string,computer:string){
-//     super(name,age,language)
-//     this.computer = computer
-//   }
-//   getLang(){
-//      console.log(`I learn ${this.language}`)
-//   }
+class WebDev extends Coder{
+
+  computer : string
+  constructor(name:string,age:number,language:string,computer:string){
+    super(name,age,language)
+    this.computer = computer
+  }
+  getLang(){
+     console.log(`I learn ${this.language}`)
+  }
   
-// }
+}
 
-// const person2 = new WebDev('Pranav',7,'TypeScript','Mac')
-// console.log(person2);
-// person2.getLang()
-
-
-// class User{
-//   name : string
-//    _age : number
-//   constructor(name:string,age:number){
-//     this.name = name
-//     this._age = age
-//   }
-//   get age(){
-//     return this._age
-//   }
-//   set age(num:number){
-//     if(num>18){
-//       this._age = num
-//     }else{
-//       console.log("Age is must be greater than 18");
-      
-//     }
-//   }
-
-// }
-
-// const myUser = new User('Prabhu',36)
-// console.log(myUser);
-// console.log(myUser.age)
-// myUser.age =17
-
-//Unknown
-// let value:unknown
-// value = 'Prabhu'
-// if(typeof value === 'string'){
-//   value = value.toUpperCase()
-// }
-// console.log(value as string)
-// let value1:string = value as string
-// console.log(value1);
-
-// function add(a:unknown){
-//   if(typeof a === 'number'){
-//     return a+2
-//   }
-//   else{
-//     return `The Value is ${a}`
-//   }
-// }
-// console.log(add(10))
-// console.log(add('Hello'))
+const person2 = new WebDev('Pranav',7,'TypeScript','Mac')
+console.log(person2);
+person2.getLang()
 
 
+class User{
+  name : string
+   _age : number
+  constructor(name:string,age:number){
+    this.name = name
+    this._age = age
+  }
+  get age(){
+    return this._age
+  }
+  set age(num:number){
+    if(num>18){
+      this._age = num 
+    }else{
+      console.log("Age is must be greater than 18");
+    }
+  }
+}
 
-// //any
-// let myName;
-// myName = 'Mani'
-// console.log(myName.toUpperCase())
-// myName =10
+const myUser = new User('Prabhu',36)
+console.log(myUser);
+console.log(myUser.age)
+myUser.age =17
+
+//  Unknown
+
+let value:unknown
+value = 'Prabhu'
+if(typeof value === 'string'){
+  value = value.toUpperCase()
+}
+console.log(value as string)
+let value1:string = value as string
+console.log(value1);
+
+function add(a:unknown){
+  if(typeof a === 'number'){
+    return a+2
+  }
+  else{
+    return `The Value is ${a}`
+  }
+}
+console.log(add(10))
+console.log(add('Hello'))
+
+//  any
+
+let myName;
+myName = 'Mani'
+console.log(myName.toUpperCase())
+myName =10
 
 //Never
 
-// function throwError():never{
-//   throw new Error('This is an Error')
-// }
+function throwError():never{
+  throw new Error('This is an Error')
+}
 
-// try{
-//   console.log(throwError());
+try{
+  console.log(throwError());
   
-// }catch(e){
-//   console.log(e.message);
+}catch(e){
+  console.log(e.message);
   
-// }
+}
 
 //as
-// let myName:unknown;
-// myName = 'Shanthi'
-// let finalName:string = myName as string
-// console.log(finalName);
+
+let myName:unknown;
+myName = 'Shanthi'
+let finalName:string = myName as string
+console.log(finalName);
 
 //any
-// let value : any;
-// value = 10
-// console.log(value);
-// value = 'Vijay'
-// console.log(value);
-// value = true
-// console.log(value);
+
+let value : any;
+value = 10
+console.log(value);
+value = 'Vijay'
+console.log(value);
+value = true
+console.log(value);
 
 //satisfies
-// type person = {
-//   name : string;
-//   age : number;
-// }
 
-// const user = {
-//   name : 'Prabhu',
-//   age : 36,
-//   isActive : true
-// } satisfies person
+type person = {
+  name : string;
+  age : number;
+}
 
-// console.log(user)
+const user = {
+  name : 'Prabhu',
+  age : 36,
+  isActive : true
+} satisfies person
+
+console.log(user)
 
 //union
-// let value:string|number;
-// value = 10
-// console.log(value);
-// value= 'Prabhu'
-// console.log(value);
+
+let value:string|number;
+value = 10
+console.log(value);
+value= 'Prabhu'
+console.log(value);
 
 //Intersection
-// type person1 = {
-//   name : string
-// }
 
-// type person2 = person1 & {
-//   age : number
-// }
+type person1 = {
+  name : string
+}
 
-// type person3 = person1 & person2 & {
-//   city : string
-// }
+type person2 = person1 & {
+  age : number
+}
 
-// const Person : person3 = {
-//   name : 'Prabhu',
-//   age : 36,
-//   city : 'Chennai'
-// }
-// console.log(Person);
+type person3 = person1 & person2 & {
+  city : string
+}
+
+const Person : person3 = {
+  name : 'Prabhu',
+  age : 36,
+  city : 'Chennai'
+}
+console.log(Person);
 
 //Aliases
-// type value = string | number
 
-// let result : value;
-// result = 10
-// console.log(result);
-// result = "Sneka"
-// console.log(result);
+type value = string | number
+
+let result : value;
+result = 10
+console.log(result);
+result = "Sneka"
+console.log(result);
 
 //keyof
-// type user = {
-//   name : string
-//   age : number
-//   isActive : true
-// }
 
-// type userKeys = keyof user
-// const user1 = "name"
-// console.log(user1);
+type user = {
+  name : string
+  age : number
+  isActive : true
+}
 
-//instanceof
-// class Bird{
-//   fly(){
-//     console.log("flying");
+type userKeys = keyof user
+const user1 = "name"
+console.log(user1);
+
+// Instanceof
+
+class Bird{
+  fly(){
+    console.log("flying");
     
-//   }
-// }
+  }
+}
 
-// const bird = new Bird()
-// console.log(bird instanceof Bird);
+const bird = new Bird()
+console.log(bird instanceof Bird);
 
 //typeof
-// let value : string | number = "Hello"
 
-// if(typeof value === 'string'){
-//   console.log('Value is String');
-// }else{
-//   console.log("Value is Number");
-// }
+let value : string | number = "Hello"
+
+if(typeof value === 'string'){
+  console.log('Value is String');
+}else{
+  console.log("Value is Number");
+}
 
 //Equality
-// let x = 10
-// let y = 20
 
-// if(x===y){
-// console.log(true);
-// }else{
-//   console.log(false);
-// }
+let x = 10
+let y = 20
+
+if(x===y){
+console.log(true);
+}else{
+  console.log(false);
+}
 
 //Truthiness
-// let value = 'prabhu'
 
-// if(value){
-//   console.log('Value Accepted');
-// }else{
-//   console.log("Value Not Accepted"); 
-// }
+let value = 'prabhu'
+
+if(value){
+  console.log('Value Accepted');
+}else{
+  console.log("Value Not Accepted"); 
+}
 
 //function overload
-// function user(name:string):string
-// function user(name:string, age:number):string
 
-// function user(name:string, age?:number):string{
-//   if(age){
-//     return `My Name is ${name} & age is ${age}`
-//   }else{
-//     return `My Name is ${name}`
-//   }
-// }
+function user(name:string):string
+function user(name:string, age:number):string
 
-// console.log(user('Prabhu'))
-// console.log(user('Prabhu',40))
+function user(name:string, age?:number):string{
+  if(age){
+    return `My Name is ${name} & age is ${age}`
+  }else{
+    return `My Name is ${name}`
+  }
+}
+
+console.log(user('Prabhu'))
+console.log(user('Prabhu',40))
 
 //Promise
-// function addTwoNumbers(num1:number,num2:number):Promise<number>{
-//   return new Promise((resolve,reject)=>{
-//     if(typeof num1==='number' && typeof num2==='number'){
-//       resolve(num1+num2)
-//     }else{
-//       reject("Both are must be Numbers")
-//     }
-//   })
-// }
 
-// addTwoNumbers(5,10)
-// .then((num)=>{
-//   console.log(`Sum is ${num}`);
-// }).catch((error)=>{
-//   console.log(error);
-  
-// })
+function addNumbers(n1:number,n2:number):Promise<number>{
+  return new Promise((resolve,reject)=>{
+    if(typeof n1 === 'number' && typeof n2 === "number"){
+      resolve (n1+n2)
+    }else{
+      reject ('Invalid creandials')
+    }
+  })
+}
+
+addNumbers(5,5)
+.then((num)=>{
+  console.log(`Sum of two numbers is ${num}`)
+})
+.catch((err)=>{
+  console.log(err)
+})
 
 //Promise converted to async
-// async function addTwoNumbers(num1:number,num2:number):Promise<number>{
-//   return new Promise((resolve,reject)=>{
-//     if(typeof num1==='number' && typeof num2==='number'){
-//       resolve(num1+num2)
-//     }else{
-//       console.log("Both are must be Numbers");
-//     }
-//   })
-// }
 
-// async function getAddition(){
-//   try{
-//     const sum = await addTwoNumbers(5,10)
-//     console.log(`Sum is ${sum}`);
-//   }catch(err){
-//     console.log(err.message);
-//   }
-// }
+function addTwoNumbers(num1: number, num2: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+      if (typeof num1 === "number" && typeof num2 === "number") {
+          resolve(num1 + num2);
+      } else {
+          reject('Invalid credentials');
+      }
+  });
+}
 
-// getAddition()
+async function sum(num1: number, num2: number): Promise<number> {
+  try {
+      const result: number = await addTwoNumbers(num1, num2);
+      return result;
+  } catch (error) {
+       console.log('Failed:', error); 
+       return 0
+  }
+}
+
+sum(1, 4).then(console.log);
+sum('a' as any, 2).then(console.log);
+
 
 //Generic
-// function Identity<T>(arg:T):T{
-//   return arg
-// }
 
-// console.log(Identity(5));
-// console.log(Identity("Hello"))
+function Identity<T>(arg:T):T{
+  return arg
+}
 
-// class Storage1<T>{
-//   _data : T
-//   constructor(initialData:T){
-//     this._data = initialData
-//   }
-//   get data():T{
-//     return this._data
-//   }
-//   set data(newData:T){
-//     this._data = newData
-//   }
-// }
+console.log(Identity(5));
+console.log(Identity("Hello"))
 
-// const num = new Storage1<number>(40)
-// console.log(num);
-// console.log(num.data);
-// num.data = 100
-// console.log(num.data);
+class Storage1<T>{
+  _data : T
+  constructor(initialData:T){
+    this._data = initialData
+  }
+  get data():T{
+    return this._data
+  }
+  set data(newData:T){
+    this._data = newData
+  }
+}
 
-// const num1 = new Storage1<string>('hello')
-// console.log(num1);
-// console.log(num1.data);
-// num1.data = 'Hiiiiiii'
-// console.log(num1.data);
+const num = new Storage1<number>(40)
+console.log(num);
+console.log(num.data);
+num.data = 100
+console.log(num.data);
 
-//Utility Types
-// interface user{
-//   name ?:string
-//   age ?:number
-// }
+const num1 = new Storage1<string>('hello')
+console.log(num1);
+console.log(num1.data);
+num1.data = 'Hiiiiiii'
+console.log(num1.data);
 
-// let myUser : Partial<user> = {name : 'Prabhu'}
 
-// let myUser1 : Required<user> = {name:'prabhu',age:36}
 
-// let myUser2 : Readonly<user> = {name:'prabhu',age:36}
+
+// Utility Types
+
+interface user{
+  name ?:string
+  age ?:number
+}
+
+let myUser : Partial<user> = {name : 'Prabhu'}
+
+let myUser1 : Required<user> = {name:'prabhu',age:36}
+
+let myUser2 : Readonly<user> = {name:'prabhu',age:36}
 
 
 
